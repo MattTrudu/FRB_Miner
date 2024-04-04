7ffa5232518a0459e996a9ae5761de477b5c6375
Matt Trudu <trudumatteo@outlook.com>
2024-04-04 19:38:08 +0200

    Added setup.py

---
+++ bin/rfi_zapper.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import numpy as np
+import argparse
+
+
+
+
+
+if __name__ == '__main__':
+    print("Ciao!")
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='FRB_Miner',
+    version='0.1',
+    packages=find_packages(),
+    scripts=['bin/rfi_flagger.py'],
+    author='Matteo Trudu',
+    author_email='matteo.trudu@inaf.it',
+    description='FRB searcher and analyser',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/MattTrudu/FRB_Miner.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
