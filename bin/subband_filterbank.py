34127cb6f57f91d68b1cd873ae963b25b29b559c
Matt Trudu <trudumatteo@outlook.com>
2024-05-20 12:11:34 -0400

    Bug fix

--- bin/subband_filterbank.py
+++ bin/subband_filterbank.py
@@ -24,7 +24,7 @@ def grab_subband(filename, outdir, outname, chanstart = 0, chanpersub = 1):
     fstart = fil.header.fch1 + chanstart*fil.header.foff
     #print(fstart)
     name = os.path.join(outdir, outname)
-    out_file = fil.header.prep_outfile(name,updates= {"nchans":chanpersub, "fch1": fstart}, nbits = fil.header.nbits)
+    out_file = fil.header.prep_outfile(name,updates= {"nchans":chanpersub, "fch1": fstart}, nbits = fil.header.nbits, rescale = True)
 
 
     for nsamps, ii, data in fil.read_plan(gulp):
