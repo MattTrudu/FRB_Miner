449e5f553e7eb368dc4e48bd82b99fb7750e8e04
Matt Trudu <trudumatteo@outlook.com>
2025-04-03 10:51:40 +0200

    bug fix

--- bin/frb_miner.py
+++ bin/frb_miner.py
@@ -91,7 +91,7 @@ def main(args):
             rficmd = rficmd + " -p"
         if iqr_filter == True:
             rficmd = rficmd + " -i"
-        if zap_chans is not None:
+        if zap_chans != 'None':
             for clo,chi in zap_chans:
                 rficmd = rficmd + f" -z {clo} {chi}"
         file.write(rficmd+"\n")
